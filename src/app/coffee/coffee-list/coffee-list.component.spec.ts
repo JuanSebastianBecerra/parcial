e7a2c6978b59edca2d0d8ce3faac66e39fbbc24e
sebastianb <juansebastianbb05@outlook.com>
2022-11-06 17:39:59 -0500

    feat: add unit test for coffee types list

--- src/app/coffee/coffee-list/coffee-list.component.spec.ts
+++ src/app/coffee/coffee-list/coffee-list.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoffeeListComponent } from './coffee-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CoffeeService} from "../coffee.service";
+import {DebugElement} from "@angular/core";
+import { faker } from '@faker-js/faker';
+import {Coffee} from "../coffee";
+import {By} from "@angular/platform-browser";
 
 describe('CoffeeListComponent', () => {
   let component: CoffeeListComponent;
   let fixture: ComponentFixture<CoffeeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoffeeListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CoffeeListComponent ],
+      providers: [CoffeeService]
     })
     .compileComponents();
   }));
@@ -16,10 +25,41 @@ describe('CoffeeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CoffeeListComponent);
     component = fixture.componentInstance;
+    for(let i = 0; i < 3; i++) {
+      const coffee = new Coffee(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence()
+      );
+      component.coffeeTypes.push(coffee);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 registers and head names on table with tr elements', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
+  });
+
+  it('should have create table columns', () => {
+    debug.queryAll(By.css('th')).forEach((th) => {
+      expect(th.attributes['scope']).toBeTruthy();
+    })
+  });
+
+  it('should have render origin coffee label', () => {
+    expect(debug.queryAll(By.css('span'))[0].nativeElement.textContent).toContain('Total café de origen');
+  });
+
+  it('should have render blend coffee label', () => {
+    expect(debug.queryAll(By.css('span'))[1].nativeElement.textContent).toContain('Total café blend');
   });
 });
--- src/app/coffee/coffee.service.spec.ts
+++ src/app/coffee/coffee.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { CoffeeService } from './coffee.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('Service: Coffee', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [CoffeeService]
     });
   });
